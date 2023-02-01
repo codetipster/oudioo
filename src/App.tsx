943a4765c114f835c23f19533d356948d7273963
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2023-02-01 14:06:45 +0100

    Add: components, audiocomponent, services, and navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from '@mui/material/Container';
+import AudioPlayer from './components/AudioPlayer';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Container >
+      <h1>hello world!</h1>
+      <AudioPlayer/>
+    </Container>
+    </>
   );
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+
+export default function MyApp() {
+  return (
+    <div>
+      <Button variant="outlined">Start</Button>
+    </div>
+  );
+}
