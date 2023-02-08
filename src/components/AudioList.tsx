1178a46f77b9ad9d3303e187958012ddef64e288
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2023-02-08 17:03:21 +0100

    Finish AudioList and AudioListItem components

--- src/components/AudioList.tsx
+++ src/components/AudioList.tsx
@@ -1,71 +1,56 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TablePagination from '@mui/material/TablePagination';
+// import TableRow from '@mui/material/TableRow';
 
 import AudioListItem from './AudioListItem'
 import { makeStyles} from '@material-ui/core/styles';
 import { Box, Typography } from '@mui/material';
 
-const useStyles = makeStyles({
-       listContainerText: {
-         fontWeight: "normal",
-         fontSize: "20px",
-         justifyContent: "space-between"
-       },
-       allTrackLink: {
-         paddingRight: "30px",
-         textDecoration: "underline",
-         
-       }
-    });
-
-
-
 //This comes from server
 const rows = [
     {
     "id": 1,
-    "title": "Kylen",
+    "title": "Kylen for nations",
     "image": "https://robohash.org/repellatreiciendisqui.png?size=50x50&set=set1",
-    "duration": "7:02 AM",
+    "duration": "07:02",
     "Artist": "Rabat",
     "source": "NA"
   }, 
   {
     "id": 2,
-    "title": "Beatriz",
+    "title": "Beatriz and the children of  yo",
     "image": "https://robohash.org/voluptatumpraesentiumsed.png?size=50x50&set=set1",
-    "duration": "7:23 PM",
+    "duration": "07:23",
     "Artist": "Woofinden",
     "source": "EU"
   }, 
   {
     "id": 3,
-    "title": "Aland",
+    "title": "Aland  and the beast",
     "image": "https://robohash.org/suscipitquamrerum.png?size=50x50&set=set1",
-    "duration": "12:43 AM",
+    "duration": "12:43 ",
     "Artist": "MacIlriach",
     "source": "AF"
   }, 
   {
     "id": 4,
-    "title": "Maurita",
+    "title": "Maurita in galactica",
     "image": "https://robohash.org/autemeaquo.png?size=50x50&set=set1",
-    "duration": "11:36 AM",
+    "duration": "11:36",
     "Artist": "Godbald",
     "source": "SA"
   }, 
   {
     "id": 5,
-    "title": "Brandtr",
+    "title": "Brandtr rock me",
     "image": "https://robohash.org/voluptatemutenim.png?size=50x50&set=set1",
-    "duration": "10:45 AM",
+    "duration": "10:45",
     "Artist": "Adacot",
     "source": "EU"
   }
@@ -73,44 +58,104 @@ const rows = [
 
 
 const AudioList = () => {
-    const classes = useStyles()
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    // const [page, setPage] = React.useState(0);
+    // const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    // const handleChangePage = (event: unknown, newPage: number) => {
+    //     setPage(newPage);
+    // };
+    
+    // const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setRowsPerPage(+event.target.value);
+    //     setPage(0);
+    // };
 
   return (
-    <Paper sx={{ width: '100%' }}>
-        <TableContainer className={classes.listContainerText} sx={{ maxHeight: 440 }}>
-          <Table stickyHeader aria-label="sticky table">
+    // <Paper sx={{ width: '100%' }}>
+    //     <TableContainer className={classes.listContainerText} sx={{ maxHeight: 440 }}>
+    //       <Table stickyHeader aria-label="sticky table">
 
-            {/* Header */}
-          <TableHead >
-            <TableRow >
-              <TableCell align="left" colSpan={1} className={classes.listContainerText}>
-                Trending Now
-              </TableCell>
-              <TableCell align="right" colSpan={1} className={classes.allTrackLink}>
-                See all
-              </TableCell>
-            </TableRow>
+    //         {/* Header */}
+    //       <TableHead >
+    //         <TableRow >
+    //           <TableCell align="left" colSpan={1} className={classes.listContainerText}>
+    //             Trending Now
+    //           </TableCell>
+    //           <TableCell align="right" colSpan={1} className={classes.allTrackLink}>
+    //             See all
+    //           </TableCell>
+    //         </TableRow>
 
-            {/* Table Rows */}
-            <TableBody> 
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                    {/* Render component for each audio track */}
-                    <AudioListItem id={row.id} title={row.title} image={row.image} duration={row.duration} artist={row.Artist}/>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </TableHead>
-          </Table>
-        </TableContainer>
-        
-        </Paper>
+    //         {/* Table Rows */}
+    //         <TableBody> 
+    //         {rows
+    //           .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+    //           .map((row) => {
+    //             return (
+    //               <TableRow hover key={row.id} >
+    //                 {/* Render component for each audio track */}
+    //                 <AudioListItem id={row.id} title={row.title} image={row.image} duration={row.duration} artist={row.Artist}/>
+    //               </TableRow>
+    //             );
+    //           })}
+    //         </TableBody>
+    //       </TableHead>
+    //       </Table>
+    //     </TableContainer>
+    //     {/* Pagination */}
+    //     <TablePagination
+    //     rowsPerPageOptions={[10, 25, 100]}
+    //     component="div"
+    //     count={rows.length}
+    //     rowsPerPage={rowsPerPage}
+    //     page={page}
+    //     onPageChange={handleChangePage}
+    //     onRowsPerPageChange={handleChangeRowsPerPage}
+    //     />
+    //     </Paper>
+
+
+<Paper sx={{ width: '100%' }}>
+<Box sx={{ maxHeight: 440 }}>
+  <Box>
+
+    {/* Header */}
+  <Box>
+    <Box sx={{display: "flex", width: "100%", justifyContent: "space-between", border: '1px solid grey'}}>
+      <Typography sx={{fontSize:"25px", padding:"20px"}}>
+        Trending right now
+      </Typography>
+      <Typography sx={{padding: "25px", textDecoration: "underline", color:"#39268d"}}>
+        See all
+      </Typography>
+    </Box>
+
+    {/* Table Rows */}
+    {rows
+      .map((row) => {
+        return (
+          <Box  key={row.id} sx={{'&:hover': {
+            backgroundColor: '#f4efef',
+          }}}>
+            {/* Render component for each audio track */}
+            <AudioListItem id={row.id} title={row.title} image={row.image} duration={row.duration} artist={row.Artist}/>
+          </Box>
+        );
+      })}  
+  </Box>
+  </Box>
+</Box>
+{/* Pagination */}
+{/* <TablePagination
+rowsPerPageOptions={[10, 25, 100]}
+component="div"
+count={rows.length}
+rowsPerPage={rowsPerPage}
+page={page}
+onPageChange={handleChangePage}
+onRowsPerPageChange={handleChangeRowsPerPage}
+/> */}
+</Paper>
     
   )
 }
